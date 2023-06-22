d1331e6742df87b8cf07243d4d710499d000dcb9
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-06-22 14:52:02 +0200

    added morgan logging

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
 
 let persons = [
     { 
@@ -95,6 +98,22 @@ if (existingNames.includes(body.name)) {
   persons = persons.concat(person)
   response.json(person)
 })
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+  app.use(requestLogger)
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
