187528b7dc77c30bd5c5486d7944e470af4def9c
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-06-23 13:24:30 +0200

    changed the fly.toml

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
+app.use(requestLogger)
 
 let notes = [
   {
