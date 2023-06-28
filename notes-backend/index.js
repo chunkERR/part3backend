0c19ab264cf79b0ff3caefae0ae15cdd612b0193
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-06-28 10:53:11 +0200

    middleware added

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -15,18 +15,25 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('build'))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).
@@ -34,13 +41,6 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
 })
-
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -59,7 +59,8 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
   .then(note => {
     if (note) {
@@ -68,18 +69,38 @@ app.get('/api/notes/:id', (request, response) => {
       response.status(404).end()
     }
   })
-  .catch(error => {
-    console.log(error)
-    response.status(500).end()
-  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
+
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
