e87b2d147313a38d14bb4a43fabf2094de4bd432
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-07-06 14:27:16 +0200

    refactoring of blog app completed

--- blog/index.js
+++ blog/index.js
@@ -1,53 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const dotenv = require('../notes-backend/utils/config')
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString();
-      delete returnedObject._id;
-      delete returnedObject.__v;
-    },
-  });
-  
-  module.exports = mongoose.model("Note", noteSchema);
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-app.use(dotenv)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
