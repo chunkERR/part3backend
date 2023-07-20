b584a261a508b05735849afe6ef19d997b557206
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-07-20 16:27:32 +0200

    created users router

--- notes-backend/app.js
+++ notes-backend/app.js
@@ -7,6 +7,7 @@ const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
 const notesRouter = require("./controllers/notes");
+const usersRouter = require('./controllers/users')
 const middleware = require("./utils/middleware");
 
 mongoose.set("strictQuery", false);
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
