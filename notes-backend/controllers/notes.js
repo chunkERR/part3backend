a689b8d1c68eda20a5b38c9d7d266c895586fe3f
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-07-21 16:30:47 +0200

    added token authorization

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -1,16 +1,30 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
   response.json(notes)
 })
 
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer')) {
+    return authorization.replace('Bearer ', '')
+  }
+return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken =jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json( { error: 'invalid token' })
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.userId)
 
   const note = new Note({
     content: body.content,
