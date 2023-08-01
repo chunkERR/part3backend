304a9bcf53403406d20245d2946a8a407a492b8b
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-01 17:46:03 +0200

    added token requirement for posting new blogs

--- part4blog/controllers/blogs.js
+++ part4blog/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog 
@@ -25,6 +34,10 @@ blogsRouter.get("/:id", (request, response, next) => {
 blogsRouter.post("/", async (request, response, next) => {
 
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
 
   const user = await User.findById(body.userId)
 
